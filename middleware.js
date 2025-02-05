bea132ef9c753945c853a0be647798f4e2bf8bce
hazardzone <tahri.mohamed.hamza@gmail.com>
2025-02-05 11:22:03 +0100

    First Push to test on SonarQube for automation

--- middleware.js
+++ middleware.js
@@ -13,6 +13,9 @@ export function middleware(request) {
     // Add security headers or other logic here
     const response = NextResponse.next();
     response.headers.set('X-Content-Type-Options', 'nosniff');
+    response.headers.set('X-Frame-Options', 'DENY');
+    response.headers.set('X-XSS-Protection', '1; mode=block');
+    response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
     return response;
   }
 
